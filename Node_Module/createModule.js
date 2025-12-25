6d0621bad51938877eb1a729e4c5df1c60b6836d
Yashvi Gor <yashvigor.ce@gmail.com>
2025-12-25 15:12:52 +0530

    Create createModule.js

---
+++ Node_Module/createModule.js
@@ -0,0 +1,7 @@
+var calc= require('./calc.js');
+// reference for refering to the module
+
+result1 =calc.add(5,5);
+result2 =calc.sub(5,1);
+console.log("The sum is "+result1);
+console.log("The sub is "+result2);
